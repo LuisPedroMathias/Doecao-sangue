e89d884bea1936eeb2679a339bc71575b436f311
LuisPedroMathias <luis_mathias@estudante.sesisenai.org.br>
2026-04-09 11:31:30 -0300

    ADD - inicio do .js

---
+++ script.js
@@ -0,0 +1,13 @@
+let doadores = [];
+
+function formulario(){
+
+    let nome= document.getElementById("nome").value
+    let email= document.getElementById("email").value
+    let idade= document.getElementById("idade").value
+    let peso= document.getElementById("tipo").value
+    let telefone= document.getElementById("Telefone").value
+    let cidade= document.getElementById("cidade").value
+    let estado= document.getElementById("estado").value
+
+}
